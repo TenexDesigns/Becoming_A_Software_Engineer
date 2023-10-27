c7a6e92cbf935bf790f5fa1ec363aac009a90a7f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-27 16:16:43 +0300

    Create 2_Journey.java

---
+++ 2_Journey.java
@@ -0,0 +1,19 @@
+Becoming a self-taught software engineer is a challenging but rewarding journey. Here are some steps and resources that can guide you:
+
+1. **Familiarize yourself with a Programming Language**: Choose a programming language based on your interests and career goals. For example, if you're interested in web development, JavaScript would be a good choice. If you're interested in data science or machine learning, Python would be better. Other popular languages include Java, C++, and C# [Source 3](https://www.edx.org/become/how-to-become-a-software-engineer-without-a-degree).
+
+2. **Learn Computer Science Basics**: Understanding data structures, algorithms, computer architecture, and software engineering principles is crucial. Websites like Coursera, edX, and Khan Academy offer free courses on these topics [Source 1](https://shaneduggan.com/self-taught-software-engineer), [Source 3](https://www.edx.org/become/how-to-become-a-software-engineer-without-a-degree).
+
+3. **Practice Coding**: Regular coding practice is essential to becoming proficient. Websites like LeetCode, HackerRank, and CodeSignal provide coding problems of varying difficulty levels [Source 4](https://emmadonery.hashnode.dev/guide-to-becoming-a-self-taught-software-engineer-best-tools-and-practices).
+
+4. **Work on Projects**: Building your own projects helps you apply what you've learned and gain practical experience. This could be anything from a simple calculator app to a complex web application.
+
+5. **Understand Software Development Processes**: Learn about Agile development, version control systems (like Git), and other software development methodologies [Source 23](https://careerkarma.com/careers/software-engineering/).
+
+6. **Join Online Communities**: Participate in communities like Stack Overflow and GitHub. These platforms can provide help when you're stuck, and they're also great places to learn from other developers' experiences.
+
+7. **Prepare for the Coding Interview**: If you're aiming to work in a tech company, you'll need to ace the coding interview. Resources like the book "Cracking the Coding Interview" and websites like LeetCode can help you prepare [Source 4](https://emmadonery.hashnode.dev/guide-to-becoming-a-self-taught-software-engineer-best-tools-and-practices).
+
+8. **Never Stop Learning**: Software engineering is a field that's constantly evolving. Continually learning new technologies and staying updated is a key part of being a software engineer.
+
+Remember, the journey to becoming a software engineer is a marathon, not a sprint. It's okay to feel overwhelmed at times. Just keep learning, practicing, and building, and you'll get there. Good luck!
