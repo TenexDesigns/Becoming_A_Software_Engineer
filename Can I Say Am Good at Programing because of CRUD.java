b40da2cb528e8283f3771afb3589c7909126d43f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-12 18:33:32 +0300

    Create Can I Say Am Good at Programing because of CRUD.java

---
+++ Can I Say Am Good at Programing because of CRUD.java
@@ -0,0 +1,58 @@
+Can I say that I am pretty good in programming if I can build a full stack CRUD web app using Python/Flask for the backend and React for the frontend?
+
+
+
+
+--------------------------------------------------------
+No. If you study tutorials on React, Flask, and Python, you will gain a basic understanding of those tools and some of the problems they solve.
+However, to get pretty good in programming, you have to show yourself able to solve real-world problems which save your clients money or earn your clients money, 
+and do it within limited time and money budgets. When you do that, your deep understanding of the tools is exposed and improved, your understanding of the problem domain
+is tested. And your ability to meet your clients’ needs with software not encoded in the framework helps highlight you as a pretty good programmer.
+
+
+
+
+
+
+--------------------------------------------------------
+CRUD is only the beginning of web programming. Other answers have already mentioned SQL optimization. What do you know of real-time messaging or websockets?
+Are you only using REST? What do you know of older protocols like SOAP or newer protocols like GraphQL? 
+What can you tell us about your proficiency with lower-level languages like C for performance-intensive tasks?
+Do you have more languages and frameworks in your toolbox?
+
+Your familiarity with CRUD doesn’t correlate to other domains such as embedded systems, machine learning, data science,
+algorithmic trading, e-commerce, or Internet of Things (among many others).
+Programming is really too broad for any individual to claim anything approaching mastery in most of its domains.
+
+
+
+-------------------------------------------------
+Its about how well one understands core concepts and how one thinks or approaches a given problem.
+
+
+
+
+
+
+----------------------------------------------------
+  to refine the requirements, design the architecture, code the application, work with testers and ship the entire application to production with confident. 
+  So your application would be clean, maintainable, expendable, scalable, testable, secure, has monitoring build in, come with CICD, and etc.
+
+
+
+
+  -----------------------------------------------------------
+  Assuming because you said CRUD, this involves database work, then yes.
+
+However, you may find that specialising in one aspect may be advantageous. 
+  Full Stack developers are starting to get a reputation as being no good at either front end or back end (I say this as a full stack developer myself).
+  However perhaps that’s a minority opinion. And after all you will mostly answer for your own reputation instead.
+
+
+
+
+
+
+
+  
+  
