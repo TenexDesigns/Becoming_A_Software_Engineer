8d63050071e6bfd353eadacbdfba5894f3275250
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-15 11:56:47 +0300

    Create 2_Design Principles.java

---
+++ Design principles in programming/2_Design Principles.java
@@ -0,0 +1,37 @@
+Design principles in programming are fundamental rules or guidelines that developers follow to create high-quality, maintainable, and efficient software. They are the cornerstone of good software development and play a crucial role in decision-making, from architectural choices to coding details.
+
+**Key Programming Design Principles:**
+
+1. **DRY (Don't Repeat Yourself):** This principle aims to eliminate repetition in code. If you find yourself writing the same code in multiple places, consider refactoring your code to make it more reusable [Source 3](https://www.spaceo.ca/blog/software-design-principles/), [Source 9](https://www.trio.dev/blog/software-design-principles).
+
+2. **KISS (Keep It Simple, Stupid):** KISS advises developers to keep their code simple and straightforward. Complex code can be hard to read, understand, and maintain [Source 3](https://www.spaceo.ca/blog/software-design-principles/), [Source 9](https://www.trio.dev/blog/software-design-principles).
+
+3. **YAGNI (You Aren't Gonna Need It):** This principle encourages developers to implement only the features and functionality they need now and not what they think they might need in the future [Source 9](https://www.trio.dev/blog/software-design-principles).
+
+4. **SOLID Principles:** SOLID is an acronym for five design principles intended to make software designs more understandable, flexible, and maintainable. They are: Single Responsibility, Open-Closed, Liskov Substitution, Interface Segregation, and Dependency Inversion [Source 3](https://www.spaceo.ca/blog/software-design-principles/), [Source 9](https://www.trio.dev/blog/software-design-principles).
+
+**Areas to Implement Design Principles:**
+
+Design principles can be applied at every level of software development:
+
+- **Architecture Design:** Design principles guide the structuring of the system into components, the arrangement of these components, and the interaction between them.
+
+- **Class Design:** Principles like SOLID guide the design of classes and interfaces, ensuring they each have a single responsibility and are decoupled from each other.
+
+- **Method Design:** Principles like DRY and KISS guide the implementation of individual methods/functions, ensuring they are simple, clear, and reusable.
+
+**Benefits of Using Design Principles:**
+
+- **Maintainability:** Code that adheres to design principles is easier to understand, modify, and extend, which leads to lower maintenance costs.
+
+- **Efficiency:** By avoiding unnecessary complexity and duplication, design principles help make code more efficient.
+
+- **Collaboration:** Design principles provide a common language for developers to discuss and review code, which improves collaboration.
+
+- **Quality:** Following design principles leads to higher code quality, which can reduce the number of bugs and make the software more reliable and robust [Source 3](https://www.spaceo.ca/blog/software-design-principles/), [Source 9](https://www.trio.dev/blog/software-design-principles).
+
+In conclusion, adhering to design principles in programming can make you a better programmer by improving the quality of your code, making it more maintainable and efficient, and making it easier for you and others to understand and extend.
+
+
+
+  ...
