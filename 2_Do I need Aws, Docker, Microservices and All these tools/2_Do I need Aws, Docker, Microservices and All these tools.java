5e340641f88382a1bb3a18e2cb80d7d89f3240ec
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-02 08:46:28 +0300

    Create 2_Do I need Aws, Docker, Microservices and All these tools.java

---
+++ 2_Do I need Aws, Docker, Microservices and All these tools/2_Do I need Aws, Docker, Microservices and All these tools.java
@@ -0,0 +1,68 @@
+I would like to be a node js backend developer and some rust for CPU bound tasks...what would be my responsibility and duties.....
+  because am a a loss of the magnitude of things out there to learn from docker, to kubernates to microservises, to Kafka to aws to system design,
+  how much work and how many hats can one wear, cant I just study how to creaat maintenance run and debug code and create solutions,
+  those other things  cause me headack and additional complexity.....I don't know if knowing all them or not makes you a good or bad backend developer, 
+  what do I need to focus on and what to avoid to not be overwhelmed, is it "Backend developer responsibilities include creating, maintaining, testing, 
+  and debugging the entire back end of an application or system. This includes the core application logic, databases, data and application integration,
+  API, and other processes taking place behind the scenes."
+
+
+
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------------------
+
+Focusing on creating, maintaining, testing, and debugging the back end is a solid foundation for a Node.js backend developer. Prioritize understanding core application logic, databases, and API development. While knowledge of Docker, Kubernetes, microservices, Kafka, AWS, and system design can be valuable, it's not mandatory initially.
+
+Start with the essentials, gradually expanding your skills based on project needs. Choose depth over breadth initially. Gain proficiency in Node.js, databases (e.g., MongoDB, PostgreSQL), and API development. As you grow, you can explore additional tools and concepts.
+
+Avoid overwhelming yourself by gradually introducing new technologies. Set clear learning goals and prioritize based on your project requirements. Collaborate with others to share the workload and learn collaboratively.
+
+Remember, becoming a backend developer is an ongoing journey. Emphasize practical application of knowledge, and don't feel pressured to master everything at once. Tailor your learning path to your interests and the demands of your projects.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+2-----> So does a backed developer need to know all the above, is such knowledge crutual or do we focus on code while the rest is. Left to the system design team,cloud engineer team, dev ops team
+  -------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+A backend developer doesnt necessarily need to be an expert in all the mentioned technologies. The depth of knowledge depends on the specific requirements of the role and the projects you're working on. In many cases, responsibilities related to system design, cloud infrastructure, and DevOps are handled by specialized teams.
+
+However, having a basic understanding of these technologies can be beneficial. It allows for better collaboration with other teams and facilitates a smoother development process. Awareness of concepts like Docker, Kubernetes, and cloud services can help you make informed decisions when designing and implementing features.
+
+As you grow in your career, you may find it valuable to explore additional technologies based on your interests and the needs of your projects. The key is to strike a balance between mastering your core skills as a backend developer and having a broader understanding of the ecosystem in which your applications operate.
+
+
+
+
+
+
+  
+
+
+
+
+
+  
+
+  
+
+
+
+
+  
